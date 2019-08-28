f05595df1cde3cfb12072b97e9fc154fd7e7d5df
Mustafa <mustafakatipoglu98@hotmail.com>
2019-08-28 15:00:37 +0300

    typedef with pointers

---
+++ 03_CodeSnippets/00_LanguageFundamentals/02_Operators/02_typedef_with_pointers/Main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+
+typedef int* intPtr;
+
+int main() {
+	
+	int *j, *k, *l;			// instead of this
+	intPtr m,n,r;			// we can do this
+
+	int *a = nullptr, *b = nullptr, *c = nullptr;		
+	intPtr x = nullptr, y = nullptr, z = nullptr;		
+	
+	std::cin.get();
+}
